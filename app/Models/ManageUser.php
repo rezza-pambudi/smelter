1cd6f7a83b9abcd9c72295125f07ef5d8da75100
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-05-03 15:22:26 +0700

    Update ManageUser.php

--- app/Models/ManageUser.php
+++ app/Models/ManageUser.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class ManageUser extends Model
 {
     use HasFactory;
-    protected $fillable = ['nik', 'nama', 'email', 'team'];
+    protected $fillable = ['nama', 'email', 'team'];
 }
