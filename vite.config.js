4b70640a7b561187ff09d7c36f6e0c679f07e704
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-05-06 14:54:18 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel, { refreshPaths } from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/**/*.blade.php','resources/**/*.js','resources/css/app.css', 'resources/js/app.js','resource/css/filament/admin/theme.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: [
                 ...refreshPaths,
                 'app/Livewire/**',
