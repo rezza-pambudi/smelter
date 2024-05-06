caca1546a30e8ed33cf9ae22e256364711c4b24e
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-05-06 14:33:16 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="{{url('/images/logo/favicon.png')}}" type="image/x-icon">
-  {{-- @vite('resources/css/app.css')
-  @vite('resources/css/theme.css')
-  @vite('resources/css/loopple/loopple.css') --}}
-  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  @vite('resources/css/app.css')
+  {{-- @vite('resources/css/theme.css') --}}
+  {{-- @vite('resources/css/loopple/loopple.css') --}}
+  {{-- <link rel="stylesheet" href="{{asset('resources/css/app.css')}}"> --}}
   <link rel="stylesheet" href="{{asset('css/theme.css')}}">
   <link rel="stylesheet" href="{{asset('css/loopple/loopple.css')}}">
 </head>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,32 @@
-import preset from './vendor/filament/support/tailwind.config.preset';
-import forms from '@tailwindcss/forms';
+// import preset from './vendor/filament/support/tailwind.config.preset';
+// import forms from '@tailwindcss/forms';
+
+// /** @type {import('tailwindcss').Config} */
+
+// export default {
+//     content: [
+//         './app/Filament/**/*.php',
+//         './resources/views/filament/**/*.blade.php',
+//         './vendor/filament/**/*.blade.php',
+//     ],
+//     theme: {
+//         extend: {},
+//       },
+//     plugins: [forms],
+// }
 
 /** @type {import('tailwindcss').Config} */
- 
 export default {
     content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
     theme: {
         extend: {},
-      },
-    plugins: [forms],
+    },
+    plugins: [],
 }
