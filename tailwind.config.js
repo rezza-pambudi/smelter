11c0c5f9eb17c9201902f536f1fc1171da780bd3
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-05-06 13:57:02 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 import preset from './vendor/filament/support/tailwind.config.preset'
-
+ 
 export default {
     presets: [preset],
     content: [
-        './pages/**/*.{html,js}',
-        './components/**/*.{html,js}',
-        './src/**/*.{html,js,php}',
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
     theme: {
         extend: {},
-    },
+      },
     plugins: [],
 }
