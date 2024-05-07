0411d661f3bd18dfa7015fb149f58e6cb83119b0
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-05-07 18:39:48 +0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,6 @@ export default {
         'tailwindcss/nesting': 'postcss-nesting',
         tailwindcss: {},
         autoprefixer: {},
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
     },
 }
