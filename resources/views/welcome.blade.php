34332ea49e762f1697b34a37229cf19c21dabb94
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-05-07 17:29:25 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="{{url('/images/logo/favicon.png')}}" type="image/x-icon">
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
   {{-- @vite('resources/css/app.css')
   @vite('resources/css/theme.css')
   @vite('resources/css/style.css')
