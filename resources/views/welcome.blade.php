1d5dded25f6bda46f599e810790fbd0594471cf5
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-05-08 01:57:54 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="{{url('/images/logo/favicon.png')}}" type="image/x-icon">
   {{-- @vite('resources/css/app.css') --}}
-  <link rel="stylesheet" href="{{asset('/build/assets/app-WwgkghjK.css')}}">
+  <link rel="stylesheet" href="{{url('/css/app-WwgkghjK.css')}}">
   <link rel="stylesheet" href="{{url('/css/theme.css')}}">
   <link rel="stylesheet" href="{{url('/css/style.css')}}">
   <link rel="stylesheet" href="{{url('/css/loopple/loopple.css')}}">
