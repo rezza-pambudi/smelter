499081b70b4b54f2b07b90eb62dd4c0cd386cb33
rezzaazaa <141853553+rezza-pambudi@users.noreply.github.com>
2024-05-07 17:27:33 +0700

    test

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -15,7 +15,7 @@
             }
         </style>
  
- @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/css/app.css')
     </head>
  
     <body class="antialiased">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,12 @@
-// import preset from './vendor/filament/support/tailwind.config.preset';
-// import forms from '@tailwindcss/forms';
-
-// /** @type {import('tailwindcss').Config} */
-
-// export default {
-//     content: [
-//         './app/Filament/**/*.php',
-//         './resources/views/filament/**/*.blade.php',
-//         './vendor/filament/**/*.blade.php',
-//     ],
-//     theme: {
-//         extend: {},
-//       },
-//     plugins: [forms],
-// }
-
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-        './app/Filament/**/*.php',
-        './resources/views/filament/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
-    ],
-    theme: {
-        extend: {
-            backgroundImage: {
-                // 'bg-wp': "url('@/resources/images/bg-wp.jpg')",
-                // 'bg-wp': "url('https://cdnstatic.detik.com/requ/bg-wp.jpg')",
-            }
-        },
-    },
-    plugins: [],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
